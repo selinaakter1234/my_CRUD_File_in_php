015a383e0a5a26b0daabdafa7f8151d6f7e2c532
selinaakter1234 <95764288+selinaakter1234@users.noreply.github.com>
2022-01-14 23:42:16 +0600

    add dropdown

--- admin/navbar.php
+++ admin/navbar.php
@@ -5,7 +5,7 @@
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="dashboard.php">dashboard</a>
         </li>
@@ -14,16 +14,26 @@
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-          <?=$_SESSION['email']?>
+              frontend
           </a>
           <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="profile.php">Profile</a></li>
+            <li><a class="dropdown-item" href="guest_msg.php">guest message <span class="badge bg-secondary">4</span> </a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item"  href="../banner.php">banner</a></li>
+          </ul>
+        </li>
+      </ul>
+      <div class="dropdown">
+          <button class="btn dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+          <?=$_SESSION['email']?>
+        </button>
+         <ul class="dropdown-menu " aria-labelledby="dropdownMenuButton1">
+           <li><a class="dropdown-item" href="profile.php">Profile</a></li>
             <li><a class="dropdown-item" href="change_password.php">change password</a></li>
             <li><hr class="dropdown-divider"></li>
             <li><a class="dropdown-item text-danger"  href="../logout.php">logout</a></li>
           </ul>
-        </li>
-      </ul>
+        </div>
       
     </div>
   </div>
